d8e2c7eccf81449f1f06dee3784d75d7f30badf1
AliReza <alirezaesm921@gmail.com>
2022-05-31 15:15:20 +0430

    search bar add

--- src/components/ContactList/contactList.js
+++ src/components/ContactList/contactList.js
@@ -5,21 +5,55 @@ import { getContact } from "../../services/getContact/getContact";
 import { deleteContact } from "../../services/deleteContact/deleteContact";
 const ContactList = () => {
   const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [allContacts, setAllContacts] = useState(null);
 
   useEffect(() => {
-    getContact()
-      .then((res) => setContacts(res.data))
-      .catch((err) => console.log(err));
-  }, [contacts]);
+    const fetchData = async () => {
+      const { data } = await getContact();
+      setContacts(data);
+      setAllContacts(data);
+    };
+    fetchData();
+  }, []);
+
+  const searchHandelr = (e) => {
+    setSearchTerm(e.target.value);
+    const search = e.target.value;
+
+    // if (search !== " ") {
+      const filteredContact = allContacts.filter((c) => {
+        return Object.values(c)
+          .join(" ")
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      });
+      // console.log(allContacts);
+      setContacts(filteredContact);
+    // } else {
+    //   setContacts(allContacts);
+    //   console.log("else ");
+    // }
+  };
 
   const deleteContactHandler = (id) => {
-    // console.log(id);
     deleteContact(id);
+    const filteredContacts = contacts.filter((c) => c.id !== id);
+    setContacts(filteredContacts);
   };
 
   return (
     <div className={styles.holderContactList}>
       <h2> Contact List </h2>
+      <div className={styles.search}>
+        <label> Search Contacts </label>
+        <input
+          type='text'
+          placeholder='Search Contact'
+          onChange={searchHandelr}
+          value={searchTerm}
+        />
+      </div>
       {contacts ? (
         contacts.map((contact) => (
           <Contact
