39823772e34d2daabc658690143d794e9a61de3d
AliReza <alirezaesm921@gmail.com>
2022-07-24 13:42:47 +0430

    change url to 3200

--- src/services/httpServis.js
+++ src/services/httpServis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-axios.defaults.baseURL = ("http://localhost:3001") ; 
+axios.defaults.baseURL = ("http://localhost:3200") ; 
 
 const http = { 
     get : axios.get ,
